28ce8305dfc876384f94e399b664527418742af0
Forrest Collins <Forest.d.collins@gmail.com>
2017-12-23 20:48:54 -0600

    Adds base encoding table

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const BaseTables = {
+  26: "abcdefghijklmnopqrstuvwxyz",
+  32: "123456789abcdefghjkmnpqrstuvwxyz", // no 0lio
+  36: "0123456789abcdefghijklmnopqrstuvwxyz",
+  49: "abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ", // no lIO
+  52: "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  58: "123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ", // no 0lIO
+  62: "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  64: "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-_"
+};
